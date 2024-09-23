83161e627c1405186cf8b91a302e66761eb4b8d0
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-23 15:29:34 +0530

    create model to manage parent category and child category

---
+++ app/Models/ChildCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChildCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        "created_by"
+    ];
+}
