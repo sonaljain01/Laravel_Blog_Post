b6bd6a620727d546bf7a708f5c440316f0d764c5
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-23 19:46:36 +0530

    update tagrequest file

--- app/Http/Requests/TagRequest.php
+++ app/Http/Requests/TagRequest.php
@@ -2,16 +2,30 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Http\FormRequest;
 
 class TagRequest extends FormRequest
 {
+    protected string $err;
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        if (!auth()->check()) {
+            return false;
+        }
+        if (!auth()->user()->type === "admin") {
+            $this->err = "You need to be admin to create tag";
+            return false;
+        }
+        return true;
+    }
+
+    protected function failedAuthorization()
+    {
+        throw new AuthorizationException($this->err);
     }
 
     /**
@@ -22,7 +36,14 @@ class TagRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => "required"
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            "name.required" => "Tag name is required"
         ];
     }
 }
