97dfe7a17417b37330f57ae1716a184e811c82d6
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-23 20:35:59 +0530

    Create a category model

---
+++ app/Models/Category.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        "name",
+        "parent_id",
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children(){
+        return $this->hasMany(Category::class,'parent_id');
+    }
+
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
+}
