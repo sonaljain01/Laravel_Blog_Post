b3766d5549eab728d9d9e2b80b43a836aaa919dd
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-23 22:32:55 +0530

    correct the migration

--- database/migrations/2024_09_23_071223_add_image_to_blogs_table.php
+++ database/migrations/2024_09_23_071223_add_image_to_blogs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('blogs', function (Blueprint $table) {
-            $table->string('image')->nullable()->after('description');
+            $table->string('image')->nullable();
         });
     }
 
