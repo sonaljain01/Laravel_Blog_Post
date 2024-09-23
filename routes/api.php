ae288fb5a3b32ae33a7864e906bc858e96264ddb
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-23 20:57:09 +0530

    update all the routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
 use App\Http\Controllers\Blog\BlogController;
+use App\Http\Controllers\Categorycontroller;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get("/",function(){
     return response()->json([
@@ -29,4 +31,16 @@ Route::group(["prefix" => "/blog"], function () {
     Route::post("/create", [BlogController::class, "store"]);
     Route::put("/update", [BlogController::class, "update"]);
     Route::delete("/delete", [BlogController::class, "destroy"]);
+})->middleware("auth:api");
+
+
+Route::get("/category", [Categorycontroller::class, "index"]);
+Route::group(["prefix" => "/category"], function () {
+    Route::post("/create", [Categorycontroller::class, "store"]);
+    Route::put("/update/{id}", [Categorycontroller::class, "update"]);
+    Route::delete("/delete/{id}", [Categorycontroller::class, "destroy"]);
+})->middleware("auth:api");
+
+Route::group(["prefix" => "/role"], function () {
+    Route::delete("/blogs/{id}", [BlogController::class, "destroy"]); 
 })->middleware("auth:api");
