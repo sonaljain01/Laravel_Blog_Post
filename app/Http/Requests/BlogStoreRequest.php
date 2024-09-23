35bd5eeb0be213d95721135af87cb948356766f6
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-23 20:40:24 +0530

    update BlogStore REquest to accept image, and tags

--- app/Http/Requests/BlogStoreRequest.php
+++ app/Http/Requests/BlogStoreRequest.php
@@ -28,6 +28,8 @@ class BlogStoreRequest extends FormRequest
             'title' => 'required|string|max:255',
             'description' => 'required|string',
             // 'user_id' => 'required|integer',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'tags' => 'nullable|string',
         ];
     }
 
@@ -38,6 +40,11 @@ class BlogStoreRequest extends FormRequest
             'title.string' => 'Title must be a string',
             'description.required' => 'Description is required',
             'description.string' => 'Description must be a string',
+            'image.image' => 'Image must be an image',
+            'image.mimes' => 'Image must be a jpeg, png, jpg, gif, svg',
+            'image.max' => 'Image is too large',
+            'tags.required' => 'Tags are required',
+            'tags.string' => 'Tags must be a string',
         ];
     }
 }
