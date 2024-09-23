7b6042223c1cf06f5e2c305bdc0185e5661be936
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-23 19:44:05 +0530

    create func to store tag

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -14,5 +14,23 @@ class TagController extends Controller
             "data" => $tags
         ], 200);
     }
+    public function store(TagRequest $request)
+    {
+        $data = [
+            "name" => $request->name,
+            "created_by" => auth()->user()->id
+        ];
+        $isSave = Tags::create($data);
+        if ($isSave) {
+            return response()->json([
+                "status" => true,
+                "message" => "Tag created successfully"
+            ], 200);
+        }
+        return response()->json([
+            "status" => false,
+            "message" => "Unable to create tag"
+        ], 500);
+    }
 
 }
