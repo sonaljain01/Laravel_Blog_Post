02fdef2ad3a292f9b3d2122025de9f5998f5ffa3
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-23 20:34:51 +0530

    Reqest file to store the category of the user

---
+++ app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if(auth()->check()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name"=> "required|string|max:255",
+            "parent_id" => "nullable|integer|exists:categories,id"
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Name is required',
+            'name.max' => 'Name is too long',
+            'name.string' => 'Name must be a string',
+            'parent_id.exists' => 'Parent id does not exist',
+            'parent_id.integer' => 'Parent id must be an integer',
+        ];
+    }
+}
