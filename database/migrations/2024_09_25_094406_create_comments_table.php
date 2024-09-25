613e8881491ff303e8b5fc05c346058d894efff9
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-25 18:02:41 +0530

    add the parent_id column in comments table to manage nested replies

--- database/migrations/2024_09_25_094406_create_comments_table.php
+++ database/migrations/2024_09_25_094406_create_comments_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parent_id')->nullable()->constrained('comments')->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('blog_id')->constrained()->cascadeOnDelete();
             $table->text('comment');
