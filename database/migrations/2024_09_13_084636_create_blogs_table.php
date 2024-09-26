65b2d3c88d93f6b173298f6f78daefeac0ebb3ed
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-26 12:55:19 +0530

    update the blogs table

--- database/migrations/2024_09_13_084636_create_blogs_table.php
+++ database/migrations/2024_09_13_084636_create_blogs_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId("user_id");
             $table->string('title');
+            // $table->stirng('slug')->unique()->nullable();
             $table->text('description');
             $table->timestamps();
         });
